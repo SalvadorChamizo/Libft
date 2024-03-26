8c86667c5d3f63bfa4e8fef7b8c2150b6c44a78f
SalvadorChamizo <139229383+SalvadorChamizo@users.noreply.github.com>
2024-03-26 12:19:53 +0100

    First upload.
    Mandatory and bonus part.

---
+++ src/ft_lstsize_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schamizo <schamizo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 15:17:38 by schamizo          #+#    #+#             */
+/*   Updated: 2024/03/26 12:00:28 by schamizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	i;
+
+	i = 0;
+	while (lst)
+	{
+		i++;
+		lst = lst->next;
+	}
+	return (i);
+}
+
+/* Cuenta el número de nodos que tiene una lista enlazada */
